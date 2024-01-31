696c82bf03c77aff7b329e882f370b6a7097da9e
dongntd <dong.nguyenthanh@powergatesoftware.com>
2024-01-31 11:22:10 +0700

    dongntd: init Chep Nee Mui project

--- src/modules/module-auth/constants/index.ts
+++ src/modules/module-auth/constants/index.ts
@@ -6,7 +6,7 @@
 
 /** constants */
 export * from './AccountState.ts';
-export * from './error.ts';
+export * from './AUTH_ERROR_CODES.ts';
 export * from './AuthScreenPath.ts';
 export * from './AuthContext.ts';
 export * from './defaultAuthState.ts';
